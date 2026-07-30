3c73db45927f4d483488ded33c04e1f5d3ce2124
Joris van den Broek <joris.vandenbroek@gmail.com>
2026-07-30 11:03:49 +0200

    chore: migrate klanten/kunstenaars schema for exclusiviteit herontwerp

--- db/schema.sql
+++ db/schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE klanten (
   invoiceCity VARCHAR(255),
   status VARCHAR(50) NOT NULL DEFAULT 'Beoordelen',
   prijsgroepId CHAR(36),
-  exclusieveKunstenaarIds JSON,
+  kunstenaarId CHAR(36),
   minimaleAfname INT,
   createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -100,9 +100,7 @@ CREATE TABLE kunstenaars (
   omschrijvingFr TEXT,
   omschrijvingDe TEXT,
   omschrijvingEn TEXT,
-  verkooprecht VARCHAR(20) NOT NULL DEFAULT 'open',
-  klantId CHAR(36),
-  exclusiefVoorKlantId CHAR(36)
+  exclusieveKlantIds JSON
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE kunstenaarAfspraken (
